5c75dfb624a2f22f31fdd0da545f878934cea59a
Minyahss <dya.yasmin@gmail.com>
2024-12-13 10:36:30 -0300

    yssmin adios

--- src/controller/DelQuartoController.java
+++ src/controller/DelQuartoController.java
@@ -3,10 +3,12 @@ package controller;
 import dao.DelQuartosDao;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import model.AddQuartos;
 import model.DelQuartos;
+import util.AlertUtil;
 
 public class DelQuartoController {
 
@@ -18,12 +20,27 @@ public class DelQuartoController {
 
     @FXML
     void Del(ActionEvent event) {
+    try {
         int id = Integer.parseInt(idID.getText());
+        String idd = idID.getText();
+
+        if (idd == null || idd.isEmpty()) {
+            throw new IllegalArgumentException("o id do cliente está vazio");
+        }
 
         DelQuartos qua = new DelQuartos(id);
 
         if (DelQuartosDao.cadastrar(qua)) {
             System.out.println("Cadastrado!");
         }
+
+    } catch (NumberFormatException e) {
+    AlertUtil.showAlert(AlertType.WARNING, "ERRO", e.getMessage());
+} catch (IllegalArgumentException e) {
+    AlertUtil.showAlert(AlertType.WARNING, "ERRO", e.getMessage());
+} catch (Exception e) {
+    AlertUtil.showAlert(AlertType.WARNING, "ERRO", e.getMessage());
+
+}
 }
 }
